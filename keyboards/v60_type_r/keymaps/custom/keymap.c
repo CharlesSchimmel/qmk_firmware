2e74479b01570cac8b11b6120ab773cbf21ead25
CharlesSchimmel <charlesschimmel@gmail.com>
2018-05-31 20:14:18 -0500

    added dvorak layout

--- keyboards/v60_type_r/keymaps/custom/keymap.c
+++ keyboards/v60_type_r/keymaps/custom/keymap.c
@@ -20,9 +20,10 @@
 #include QMK_KEYBOARD_H
 
 // LAYER DEFs
-#define _NAV 1
-#define _NUMPAD 2
-#define _FUNC 3
+#define _DVORAK 1
+#define _NAV 2
+#define _NUMPAD 3
+#define _FUNC 4
 
 #define _____ KC_TRNS
 #define XXXXX KC_NO
@@ -55,7 +56,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,   \
 	       KC_LCTL,       KC_LGUI, KC_LALT,          LT(_NAV,KC_SPC),                         MO(_FUNC),   KC_RGUI, KC_APP,  KC_RCTL),
 
-  /* Keymap 1: Navigation, VIM
+  /* Keymap 1: Default Layer (DVORAK)
+   * ,-----------------------------------------------------------.
+   * | ` |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|   Bs  |
+   * |-----------------------------------------------------------|
+   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
+   * |-----------------------------------------------------------|
+   * |Esc/Ctl|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |-----------------------------------------------------------|
+   * |LShift  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| RShift   |
+   * |-----------------------------------------------------------|
+   * |Ctrl|Gui |Alt |      Space,NUM          |Fn0 |Gui |App|Ctrl|
+   * `-----------------------------------------------------------'
+   */
+  [_DVORAK] = LAYOUT_60_ansi(
+	       KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC,  KC_BSPC, \
+	       KC_TAB,        KC_QUOT,    KC_COMM,    KC_DOT,    KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL, KC_BSLS, \
+	       CTL_T(KC_ESC), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_MINS,          KC_ENT,  \
+	       KC_LSFT,       KC_SCLN,   KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W, KC_V,  KC_Z,          KC_RSFT,   \
+	       KC_LCTL,       KC_LGUI, KC_LALT,          LT(_NAV,KC_SPC),                         MO(_FUNC),   KC_RGUI, KC_APP,  KC_RCTL),
+
+  /* Keymap 2: Navigation, VIM
    * Parent: Default
    *	Actvtr: Space
    * Chldrn: Numpad (LCtrl)
@@ -78,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       _____,   _____,  _____,  _____,  _____,  VIM_BACK, _____, _____, KC_VOLD,  KC_VOLU,  KC_MUTE,           _____,  \
 	       MO(_NUMPAD),   _____,  _____,            _____,                                                           _____,  _____, _____, _____),
 
-  /* Keymap 2: Numpad Layer
+  /* Keymap 3: Numpad Layer
    * Parent: Navigation
    *	Actvtr: LCtrl
    * Chldrn: None
@@ -102,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       _____,       _____,    _____,    _____,    _____,    KC_KP_0,    KC_KP_1,    KC_KP_2,    KC_KP_3, KC_DOT,  KC_SLSH,          _____,   \
 	       _____,       _____, _____,          _____,                                      _____,   _____, _____,  _____),
 
-  /* Keymap 3: Function, Media
+  /* Keymap 4: Function, Media
    * Parent: Default
    *	Actvtr: Fn0
    * Chldrn: None
@@ -120,9 +141,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_FUNC] = LAYOUT_60_ansi(
 	       KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _____, \
-	       _____,   _____,  _____,     _____,  _____,  _____,  _____,   _____, _____, _____, KC_MPLY, KC_MPRV,    KC_MNXT,          _____, \
-	       KC_CAPS,   _____,  _____,  _____,  _____,  _____,  _____,   _____,   _____, _____, _____, _____,          _____,          \
-	       _____,   _____,  _____,  _____,  _____,  _____, _____, KC_MUTE, KC_VOLD,  KC_VOLU,  _____,           _____,  \
+	       _____,   DF(0),  _____,     _____,  _____,  _____,  _____,   _____, _____, _____, KC_MPLY, KC_MPRV,    KC_MNXT,          _____, \
+	       KC_CAPS,   _____,  _____,  DF(_DVORAK),  _____,  _____,  DF(_DVORAK),   _____,   _____, _____, _____, _____,          _____,          \
+	       _____,   _____,  DF(0),  _____,  _____,  _____, _____, KC_MUTE, KC_VOLD,  KC_VOLU,  _____,           _____,  \
 	       _____,   _____,  _____,            _____,                                                           _____,  _____, _____, _____),
 };
 
