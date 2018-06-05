7e510ee4c8d39670aa5714928d686e7c1d835c90
CharlesSchimmel <charlesschimmel@gmail.com>
2018-06-04 19:55:59 -0500

    Removed dvorak layer (better to do it in software imo)

--- keyboards/v60_type_r/keymaps/custom/keymap.c
+++ keyboards/v60_type_r/keymaps/custom/keymap.c
@@ -20,10 +20,9 @@
 #include QMK_KEYBOARD_H
 
 // LAYER DEFs
-#define _DVORAK 1
-#define _NAV 2
-#define _NUMPAD 3
-#define _FUNC 4
+#define _NAV 1
+#define _NUMPAD 2
+#define _FUNC 3
 
 #define _____ KC_TRNS
 #define XXXXX KC_NO
@@ -46,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |LShift  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| RShift   |
    * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space,NUM          |Fn0 |Gui |App|Ctrl|
+   * |Ctrl|Alt |Gui |      Space,NUM          |Fn0 |Gui |App|Ctrl|
    * `-----------------------------------------------------------'
    */
   [0] = LAYOUT_60_ansi(
@@ -54,29 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
 	       CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
 	       KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,   \
-	       KC_LCTL,       KC_LGUI, KC_LALT,          LT(_NAV,KC_SPC),                         MO(_FUNC),   KC_RGUI, KC_APP,  KC_RCTL),
+	       KC_LCTL,       KC_LALT, KC_LGUI,          LT(_NAV,KC_SPC),                         MO(_FUNC),   KC_RGUI, KC_APP,  KC_RCTL),
 
-  /* Keymap 1: Default Layer (DVORAK)
-   * ,-----------------------------------------------------------.
-   * | ` |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|   Bs  |
-   * |-----------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
-   * |-----------------------------------------------------------|
-   * |Esc/Ctl|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
-   * |-----------------------------------------------------------|
-   * |LShift  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| RShift   |
-   * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space,NUM          |Fn0 |Gui |App|Ctrl|
-   * `-----------------------------------------------------------'
-   */
-  [_DVORAK] = LAYOUT_60_ansi(
-	       KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC,  KC_BSPC, \
-	       KC_TAB,        KC_QUOT,    KC_COMM,    KC_DOT,    KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL, KC_BSLS, \
-	       CTL_T(KC_ESC), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_MINS,          KC_ENT,  \
-	       KC_LSFT,       KC_SCLN,   KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W, KC_V,  KC_Z,          KC_RSFT,   \
-	       KC_LCTL,       KC_LGUI, KC_LALT,          LT(_NAV,KC_SPC),                         MO(_FUNC),   KC_RGUI, KC_APP,  KC_RCTL),
-
-  /* Keymap 2: Navigation, VIM
+  /* Keymap 1: Navigation, VIM
    * Parent: Default
    *	Actvtr: Space
    * Chldrn: Numpad (LCtrl)
@@ -99,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       _____,   _____,  _____,  _____,  _____,  VIM_BACK, _____, _____, KC_VOLD,  KC_VOLU,  KC_MUTE,           _____,  \
 	       TG(_NUMPAD),   _____,  _____,            _____,                                                           _____,  _____, _____, _____),
 
-  /* Keymap 3: Numpad Layer
+  /* Keymap 2: Numpad Layer
    * Parent: Navigation
    * Chldrn: None
    * ,-----------------------------------------------------------.
@@ -122,11 +101,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       _____,       _____,    _____,    _____,    _____,    KC_0,    KC_1,    KC_2,    KC_3, KC_DOT,  KC_SLSH,          _____,   \
 	       TG(_NUMPAD),       _____, _____,          _____,                                      _____,   _____, _____,  _____),
 
-  /* Keymap 4: Function, Media
+  /* Keymap 3: Function, Media
    * Parent: Default
    * Chldrn: None
    * ,-----------------------------------------------------------.
-   * | ` | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|       |
+   * | ` | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12| Del   |
    * |-----------------------------------------------------------|
    * |     |   |   |   |   |   |   |   |PSc|SLk|Pau|<<|>> |      |
    * |-----------------------------------------------------------|
@@ -138,10 +117,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
   [_FUNC] = LAYOUT_60_ansi(
-	       KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _____, \
-	       _____,   DF(0),  _____,     _____,  _____,  _____,  _____,   _____, KC_PSCR, KC_SLCK, KC_MPLY, KC_MPRV,    KC_MNXT,          _____, \
-	       KC_CAPS,   _____,  _____,  DF(_DVORAK),  _____,  _____,  DF(_DVORAK),   _____,   _____, _____, _____, _____,          _____,          \
-	       _____,   _____,  DF(0),  _____,  _____,  _____, _____, KC_MUTE, KC_VOLD,  KC_VOLU,  _____,           _____,  \
+	       KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL, \
+	       _____,   _____,  _____,     _____,  _____,  _____,  _____,   _____, KC_PSCR, KC_SLCK, KC_MPLY, KC_MPRV,    KC_MNXT,          _____, \
+	       KC_CAPS,   _____,  _____,  _____,  _____,  _____,  _____,   _____,   _____, _____, _____, _____,          _____,          \
+	       _____,   _____,  _____,  _____,  _____,  _____, _____, KC_MUTE, KC_VOLD,  KC_VOLU,  _____,           _____,  \
 	       _____,   _____,  _____,            _____,                                                           _____,  _____, _____, _____),
 };
 
