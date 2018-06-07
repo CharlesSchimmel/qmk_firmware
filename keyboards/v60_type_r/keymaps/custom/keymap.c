8233c7248a0084f1b2e5235350aa8f13ff0f4ffe
CharlesSchimmel <charlesschimmel@gmail.com>
2018-06-06 20:34:14 -0500

    Added Alt_T(Enter) to enter

--- keyboards/v60_type_r/keymaps/custom/keymap.c
+++ keyboards/v60_type_r/keymaps/custom/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
    * |-----------------------------------------------------------|
-   * |Esc/Ctl|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |Esc/Ctl|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Alt/Rt |
    * |-----------------------------------------------------------|
    * |LShift  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| RShift   |
    * |-----------------------------------------------------------|
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_60_ansi(
 	       KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
 	       KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-	       CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
+	       CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          ALT_T(KC_ENT ),  \
 	       KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,   \
 	       KC_LCTL,       KC_LALT, KC_LGUI,          LT(_NAV,KC_SPC),                         MO(_FUNC),   KC_RGUI, KC_APP,  KC_RCTL),
 
@@ -103,7 +103,7 @@ l  */
    * |-----------------------------------------------------------|
    * |     |   |   |   |   |   |   |   |PSc|SLk|Pau|<<|>> |      |
    * |-----------------------------------------------------------|
-   * | Caps |   |   |   |   |   |   |   |   |   |Itj|   |        |
+   * | Caps |   |   |   |   |   |   |   |   |   |   |   |        |
    * |-----------------------------------------------------------|
    * |        |   |   |   |   |    |    |   |VlD|VlU|Mut|        |
    * |-----------------------------------------------------------|
@@ -113,7 +113,7 @@ l  */
   [_FUNC] = LAYOUT_60_ansi(
 	       KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL, \
 	       _____,   _____,  _____,     _____,  _____,  _____,  _____,   _____, KC_PSCR, KC_SLCK, KC_MPLY, KC_MPRV,    KC_MNXT,          _____, \
-	       KC_CAPS,   _____,  _____,  _____,  _____,  _____,  _____,   _____,   _____, _____, MY_CUSTOM_MACRO, _____,          _____,          \
+	       KC_CAPS,   _____,  _____,  _____,  _____,  _____,  _____,   _____,   _____, _____, _____, _____,          KC_ENT,          \
 	       _____,   _____,  _____,  _____,  _____,  _____, _____, KC_MUTE, KC_VOLD,  KC_VOLU,  _____,           _____,  \
 	       _____,   _____,  _____,            _____,                                                           _____,  _____, _____, TG(_NUMPAD)),
 };
