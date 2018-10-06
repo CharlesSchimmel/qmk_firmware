ee3c0b082a7f46ccd997ae09367adb65800aba1b
CharlesSchimmel <charlesschimmel@gmail.com>
2018-10-06 11:24:26 -0500

    aliases

--- keyboards/v60_type_r/keymaps/custom/keymap.c
+++ keyboards/v60_type_r/keymaps/custom/keymap.c
@@ -26,7 +26,9 @@
 
 // Aliases
 #define SH_BSP MT(MOD_RSFT, KC_BSPC)
-#define SH_DEL MT(MOD_RSFT, SH_DEL)
+#define SH_DEL MT(MOD_RSFT, KC_DEL)
+#define CT_ESC CTL_T(KC_ESC)
+#define AL_ENT ALT_T(KC_ENT)
 
 #define _____ KC_TRNS
 #define XXXXX KC_NO
@@ -49,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_60_ansi(
 	       KC_GESC,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,         KC_BSPC, \
 	       KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC,        KC_BSLS, \
-	       CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, ALT_T(KC_ENT ), \
+	       CT_ESC,		  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, AL_ENT, \
 	       KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, SH_BSP, \
 	       KC_LCTL,       KC_LALT, KC_LGUI, LT(_NAV, KC_SPC), MO(_FUNC), KC_RGUI, KC_APP, KC_RCTL
 		   ),
