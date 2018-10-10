936d1c1ae843ece457a202706e7ecaa1c1528385
CharlesSchimmel <charlesschimmel@gmail.com>
2018-10-09 19:18:11 -0500

    move media keys, sh_slsh

--- keyboards/v60_type_r/keymaps/custom/keymap.c
+++ keyboards/v60_type_r/keymaps/custom/keymap.c
@@ -29,8 +29,11 @@
 #define SH_DEL MT(MOD_RSFT, KC_DEL)
 #define CT_ESC CTL_T(KC_ESC)
 #define AL_ENT ALT_T(KC_ENT)
-#define CT_QOT LCTL_T(KC_QUOT)
-#define AL_A RALT_T(KC_A)
+#define SH_SLSH MT(MOD_RSFT, KC_SLSH)
+
+// Layer Taps
+#define FN_MNU LT(_FUNC, KC_APP)
+#define SP_NAV LT(_NAV, KC_SPC)
 
 #define _____ KC_TRNS
 #define XXXXX KC_NO
@@ -41,21 +44,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------.
    * | ` | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = |   Bs  |
    * |-----------------------------------------------------------|
-   * |Tab  | Q | W | E | R | T | Y | U | I | O | P | [ | ] |  \  |
+   * | Tab | Q | W | E | R | T | Y | U | I | O | P | [ | ] |  \  |
    * |-----------------------------------------------------------|
-   * |Esc/Ctl| A | S | D | F | G | H | J | K | L | ; | ' |Alt/Ent|
+   * | EsCtl | A | S | D | F | G | H | J | K | L | ; | ' | AlEnt |
    * |-----------------------------------------------------------|
-   * |LShift  | Z | X | C | V | B | N | M | , | . | / |  SftBsp  |
+   * | Shift  | Z | X | C | V | B | N | M | , | . | / |  SftBsp  |
    * |-----------------------------------------------------------|
-   * |Ctrl|Alt |Gui |         Space           |Nav |Fn3 |Mnu|Ctrl|
+   * |Ctrl|Alt |Gui |         Space           |Nav |Gui |Mnu|Ctrl|
    * `-----------------------------------------------------------'
    */
   [0] = LAYOUT_60_ansi(
-	       KC_GESC,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,         KC_BSPC, \
-	       KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC,        KC_BSLS, \
-	       CT_ESC,		  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, AL_ENT, \
-	       KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, SH_BSP, \
-	       KC_LCTL,       KC_LALT, KC_LGUI, LT(_NAV, KC_SPC), MO(_FUNC), KC_RGUI, KC_APP, KC_RCTL
+	       KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,    KC_6,    KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+	       KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,    KC_Y,    KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
+	       CT_ESC,  KC_A,    KC_S,    KC_D,   KC_F,   KC_G,    KC_H,    KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, AL_ENT,  \
+	       KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT, SH_SLSH, SH_BSP,  \
+	       KC_LCTL, KC_LALT, KC_LGUI, SP_NAV, FN_MNU, KC_RGUI, KC_RALT, KC_RCTL
 		   ),
 
   /* Keymap 1: Navigation, VIM
@@ -69,16 +72,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |      |   |   |   |   |   | h | j | k | l |   |   |        |
    * |-----------------------------------------------------------|
-   * |        |   |   |   |   |    |    |   |VlD|VlU|Mut| SftDel |
+   * |        |   |   |   |   |    |    |   |   |   |   | SftDel |
    * |-----------------------------------------------------------|
-   * |    |    |    |                        |    |    |    |NumP|
+   * |    |    |    |          XXXX          |    |    |    |NumP|
    * `-----------------------------------------------------------'
    */
   [_NAV] = LAYOUT_60_ansi(
-	       _____, _____, _____, _____, KC_END, _____, _____,   _____,        _____,   _____,   KC_HOME, _____,   _____,   KC_DEL, \
-	       _____, _____, _____, _____, _____,  _____, _____,   _____,        _____,   _____,   _____,   KC_PGUP, KC_PGDN, KC_INS, \
-	       _____, _____, _____, _____, _____,  _____, KC_LEFT, KC_DOWN,      KC_UP,   KC_RGHT, _____,   _____,   _____,   \
-	       _____, _____, _____, _____, _____,  _____, _____,   _____,        KC_VOLD, KC_VOLU, KC_MUTE, SH_DEL,   \
+	       _____, _____, _____, _____, KC_END, _____, _____,   _____,   _____, _____,   KC_HOME, _____,   _____,   KC_DEL, \
+	       _____, _____, _____, _____, _____,  _____, _____,   _____,   _____, _____,   _____,   KC_PGUP, KC_PGDN, KC_INS, \
+	       _____, _____, _____, _____, _____,  _____, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _____,   _____,   _____,   \
+	       _____, _____, _____, _____, _____,  _____, _____,   _____,   _____, _____,   _____,   SH_DEL,  \
 	       _____, _____, _____, _____, _____,  _____, _____,   TG(_NUMPAD)
 		   ),
 
@@ -112,21 +115,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------.
    * | ` | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12| Del   |
    * |-----------------------------------------------------------|
-   * |     |   |   |   |   |   |   |   |PSc|SLk|Pau|<<|>> |      |
+   * |     |   |   |   |   |   |   |   |PSc|SLk|   |  |   |      |
    * |-----------------------------------------------------------|
    * |      |   |   |   |   |   |   |   |   |   |   |   |        |
    * |-----------------------------------------------------------|
-   * |        |   |   |   |   |    |    |   |VlD|VlU|Mut|        |
+   * |        |   |   |   |   |MPrv|MNxt|Mut|VlD|VlU|Ply| SftDel |
    * |-----------------------------------------------------------|
-   * |    |    |    |                        |    |    |Caps|NumP|
+   * |    |    |    |                        |XXXX|    |Caps|NumP|
    * `-----------------------------------------------------------'
    */
   [_FUNC] = LAYOUT_60_ansi(
-	       KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,   KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
-	       _____,  _____, _____, _____, _____, _____, _____,   _____, KC_PSCR, KC_SLCK, KC_MPLY, KC_MPRV, KC_MNXT, _____,  \
-	       _____,  _____, _____, _____, _____, _____, _____,   _____, _____,   _____,   _____,   _____,   KC_ENT,  \
-	       _____,  _____, _____, _____, _____, _____, _____,   _____, KC_VOLD, KC_VOLU, KC_MUTE, _____,   \
-	       _____,  _____, _____, _____, _____, _____, KC_CAPS, TG(_NUMPAD)
+	       KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12, KC_DEL, \
+	       _____,  _____, _____, _____, _____, _____,   _____,   _____,   KC_PSCR, KC_SLCK, _____,   _____,  _____,  _____,  \
+	       _____,  _____, _____, _____, _____, _____,   _____,   _____,   _____,   _____,   _____,   _____,  KC_ENT, \
+	       _____,  _____, _____, _____, _____, KC_MPRV, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, SH_DEL, \
+	       _____,  _____, _____, _____, _____, _____,   KC_CAPS, TG(_NUMPAD)
 		   ),
 };
 
@@ -137,15 +140,15 @@ const uint16_t PROGMEM fn_actions[] = {
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
-      }
+      /* switch(id) { */
+      /*   case 0: */
+      /*     if (record->event.pressed) { */
+      /*       register_code(KC_RSFT); */
+      /*     } else { */
+      /*       unregister_code(KC_RSFT); */
+      /*     } */
+      /*   break; */
+      /* } */
     return MACRO_NONE;
 };
 
