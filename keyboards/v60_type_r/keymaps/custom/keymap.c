b41853b996c665239a3abb43d9cc520938fc5e5f
CharlesSchimmel <charlesschimmel@gmail.com>
2018-10-10 20:08:40 -0500

    sh_slsh -> kc_slsh and formatting fauxrty visual layout

--- keyboards/v60_type_r/keymaps/custom/keymap.c
+++ keyboards/v60_type_r/keymaps/custom/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,    KC_6,    KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
 	       KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,    KC_Y,    KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
 	       CT_ESC,  KC_A,    KC_S,    KC_D,   KC_F,   KC_G,    KC_H,    KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, AL_ENT,  \
-	       KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT, SH_SLSH, SH_BSP,  \
+	       KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT, KC_SLSH, SH_BSP,  \
 	       KC_LCTL, KC_LALT, KC_LGUI, SP_NAV, FN_MNU, KC_RGUI, KC_RALT, KC_RCTL
 		   ),
 
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |     |mwu|lc | u |rc |   |   |   |PSc|SLk|   |  |   |      |
    * |-----------------------------------------------------------|
-   * |      |mwd| l | d | r    |   |   |   |   |   |   |        |
+   * |      |mwd| l | d | r |   |   |   |   |   |   |   |        |
    * |-----------------------------------------------------------|
    * |        |   |   |   |   |MPrv|MNxt|Mut|VlD|VlU|Ply| SftDel |
    * |-----------------------------------------------------------|
