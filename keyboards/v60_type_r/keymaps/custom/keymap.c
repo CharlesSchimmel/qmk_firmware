d2ba95f30f75e3021ffb6ecc4a37f3380349c35a
CharlesSchimmel <charlesschimmel@gmail.com>
2019-03-06 22:12:22 -0600

    blah

--- keyboards/v60_type_r/keymaps/custom/keymap.c
+++ keyboards/v60_type_r/keymaps/custom/keymap.c
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------.
    * |   |   |   |   |End|   |   |   |   |   |Hom|   |   |  Del  |
    * |-----------------------------------------------------------|
-   * |     |wu |lc | u | rc|   |Ynk|   |   |   |Put|PU|PgD| Ins  |
+   * |     |wu |lc | u | rc|   |   |   |   |   |   |PU|PgD| Ins  |
    * |-----------------------------------------------------------|
    * |      |wd | l | d | r |   | h | j | k | l |   |   |        |
    * |--------------------.-----------.--------------------------|
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_NAV] = LAYOUT_60_ansi(
 	       _____, _____,         _____,      _____,      KC_END,      _____, _____,   _____,   _____, _____,   KC_HOME, _____,   _____,   KC_DEL, \
-	       _____, KC_MS_WH_UP,   KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2,  _____, M_YNK,   _____,   _____, _____,   M_PUT,   KC_PGUP, KC_PGDN, KC_INS, \
+	       _____, KC_MS_WH_UP,   KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2,  _____, _____,   _____,   _____, _____,   _____,   KC_PGUP, KC_PGDN, KC_INS, \
 	       _____, KC_MS_WH_DOWN, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, _____, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _____,   _____,   _____,   \
 	       _____, _____,         _____,      _____,      _____,       _____, _____,   _____,   _____, _____,   _____,   SH_DEL,  \
 	       _____, _____,         _____,      _____,      _____,       _____, _____,   TG(_NUM)
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------.
    * | ` | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  Del  |
    * |-----------------------------------------------------------|
-   * |     |LOK|CAD|   |RAD|DEF|   |   |   |   |Ply|I<| >I|      |
+   * |     |LOK|CAD|   |   |   |   |   |   |   |Ply|I<| >I|      |
    * |-----------------------------------------------------------|
    * |      |   |   |   |   |   |   |   |   |   |   |   |        |
    * |--------------------.-----------.--------------------------|
@@ -116,11 +116,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
   [_FNC] = LAYOUT_60_ansi(
-	       KC_GRV, KC_F1,   KC_F2,   KC_F3, KC_F4,    KC_F5,    KC_F6,   KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
-	       _____,  M_LOK,   M_CAD,   _____, DF(_RAD), DF(_DEF), _____,   _____, _____,   _____,   KC_MPLY, KC_MPRV, KC_MNXT, _____,  \
-	       _____,  _____,   _____,   _____, _____,    _____,    _____,   _____, _____,   _____,   _____,   _____,   _____,   \
-	       _____,  KC_PSCR, KC_SLCK, _____, _____,    _____,    _____,   _____, KC_VOLD, KC_VOLU, KC_MUTE, SH_DEL,  \
-	       _____,  _____,   _____,   _____, _____,    _____,    KC_CAPS, TG(_NUM)
+	       KC_GRV, KC_F1,   KC_F2,   KC_F3, KC_F4, KC_F5, KC_F6,   KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
+	       _____,  M_LOK,   M_CAD,   _____, _____, _____, _____,   _____, _____,   _____,   KC_MPLY, KC_MPRV, KC_MNXT, _____,  \
+	       _____,  _____,   _____,   _____, _____, _____, _____,   _____, _____,   _____,   _____,   _____,   _____,   \
+	       _____,  KC_PSCR, KC_SLCK, _____, _____, _____, _____,   _____, KC_VOLD, KC_VOLU, KC_MUTE, SH_DEL,  \
+	       _____,  _____,   _____,   _____, _____, _____, KC_CAPS, TG(_NUM)
 		   ),
 
   /* Keymap 3: Numpad Layer
@@ -144,27 +144,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       _____, _____, _____, _____, _____, _____, KC_4,  KC_5, KC_6, _____,  _____,   _____,   _____,  \
 	       _____, _____, _____, _____, _____, KC_0,  KC_1,  KC_2, KC_3, KC_DOT, KC_SLSH, _____,   \
 	       _____, _____, _____, _____, _____, _____, _____, TG(_NUM)
-		   ),
-
-  /* Keymap 4: "RAD" Layer - More ergonomic modifiers
-   * ,-----------------------------------------------------------.
-   * | ` | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = |   Bs  |
-   * |-----------------------------------------------------------|
-   * | LTab| Q | W | E | R | T | Y | U | I | O | P | [ | ] | \lt |
-   * |-----------------------------------------------------------|
-   * | ShTab | A | S | D | F | G | H | J | K | L | ; | ' | SfBsp |
-   * |---------------------.-----------.-------------------------|
-   * | EsCtl  | Z | X | C | V | B | N | M | , | . | / |  CtlEnt  |
-   * |-----------------------------------------------------------|
-   * |Ctrl|Alt |Gui |         SpNav           |Sym | Fn |Alt|Ctrl|
-   * `-----------------------------------------------------------'
-   */
-  [_RAD] = LAYOUT_60_ansi(
-	       KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
-	       AL_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, AL_BSLS, \
-	       SH_TAB,  KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, SH_BSP,  \
-	       CT_ESC,  KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT, KC_SLSH, CT_ENT,  \
-	       KC_LCTL, KC_LALT, KC_LGUI, SP_NAV, MNU_FNC, KC_RGUI, KC_RALT, KC_RCTL
 		   ),
 
 };
