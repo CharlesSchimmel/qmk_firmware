fbb031badb72ef86381f234f42b039f617c7949e
CharlesSchimmel <charlesschimmel@gmail.com>
2019-03-16 19:11:47 -0500

    swap numbers and brackets on symbol

--- keyboards/v60_type_r/keymaps/custom/keymap.c
+++ keyboards/v60_type_r/keymaps/custom/keymap.c
@@ -140,8 +140,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_SYM] = LAYOUT_60_ansi(
 	       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,   \
-	       _____,   KC_LBRC, KC_LCBR, KC_LPRN, KC_EQL,  _____,   _____,   KC_MINS, KC_RPRN, KC_RCBR, KC_RBRC, _____,   _____,   _____,    \
-	       _____,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,  _____,             \
+	       _____,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,  _____,   _____,    \
+	       _____,   KC_LBRC, KC_LCBR, KC_LPRN, KC_EQL,  _____,   _____,   KC_MINS, KC_RPRN, KC_RCBR, KC_RBRC, _____,   _____,             \
 	       _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,                      \
 	       _____,   _____,   _____,   _____,   XXXXX,   _____,   _____,   _____
 		   )
