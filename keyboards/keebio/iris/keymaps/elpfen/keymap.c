1645a96876b412106439e54209ce61b7bb79d083
CharlesSchimmel <charlesschimmel@gmail.com>
2021-08-01 17:48:01 -0500

    working again: state is bad and types are good

--- keyboards/keebio/iris/keymaps/elpfen/keymap.c
+++ keyboards/keebio/iris/keymaps/elpfen/keymap.c
@@ -92,14 +92,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool m_layer_lock(uint16_t current_keycode, keyrecord_t *record, bool *lock_flag, uint16_t target_keycode, uint16_t layer) {
     if (current_keycode != target_keycode || !record->event.pressed) return true;
-    SEND_STRING(SS_TAP(X_M));
 
-    if (lock_flag) {
-        SEND_STRING(SS_TAP(X_Y));
+    if (*lock_flag) {
         layer_off(layer);
         *lock_flag = false;
     } else {
-        SEND_STRING(SS_TAP(X_N));
         *lock_flag = true;
     }
 
@@ -144,14 +141,12 @@ static td_tap_t ql_tap_state = {
 };
 
 void td_layer_lock_finished(qk_tap_dance_state_t *state, uint16_t keycode, uint16_t layer, bool *lock_flag) {
-    switch (cur_dance(state)) {
+    ql_tap_state.state = cur_dance(state);
+    switch (ql_tap_state.state) {
         case TD_SINGLE_TAP:
-            SEND_STRING(SS_TAP(X_A));
             tap_code(keycode);
             break;
         case TD_SINGLE_HOLD:
-            *lock_flag = false;
-            SEND_STRING(SS_TAP(X_B));
             layer_on(layer);
             break;
         default: break;
@@ -166,18 +161,15 @@ void ZMO_NAV_finished(qk_tap_dance_state_t *state, void *user_data) {
     td_layer_lock_finished(state, KC_Z, _NAV, &nav_lock);
 }
 
-void td_layer_lock_reset(qk_tap_dance_state_t *state, bool lock_flag, uint16_t layer) {
-    SEND_STRING(SS_TAP(X_R));
-    if (ql_tap_state.state == TD_SINGLE_HOLD && !lock_flag) {
+void td_layer_lock_reset(qk_tap_dance_state_t *state, bool *lock_flag, uint16_t layer) {
+    if (ql_tap_state.state == TD_SINGLE_HOLD && !*lock_flag) {
         layer_off(layer);
-        SEND_STRING(SS_TAP(X_N));
     }
-    SEND_STRING(SS_TAP(X_Y));
     ql_tap_state.state = TD_NONE;
 }
 
 void nav_lock_reset(qk_tap_dance_state_t *state, void *user_data) {
-    td_layer_lock_reset(state, nav_lock, _NAV);
+    td_layer_lock_reset(state, &nav_lock, _NAV);
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
