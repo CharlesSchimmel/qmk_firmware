22ca74e71b9e132354baed537f0a596e5ee97856
CharlesSchimmel <charlesschimmel@gmail.com>
2021-08-06 15:57:56 -0500

    start adding sym layer to userspace

--- users/elpfen/elpfen.h
+++ users/elpfen/elpfen.h
@@ -89,6 +89,8 @@ void send_alt_f4(void);
 #define BTMR1 KC_RGUI
 #define BTMR2 FN_MNU
 
+#define ________________SYM_2U_Sides_______________ KC_F12, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+
 #define _____________Base_Bottom_Center____________ BTML2, BTML1, BTML0, BTMR0, BTMR1, BTMR2
 
 #define _____________Dvorak_2Up_Core_______________ KC_1,    KC_2,    KC_3,    KC_4,   KC_5, KC_6, KC_7, KC_8, KC_9, KC_0
