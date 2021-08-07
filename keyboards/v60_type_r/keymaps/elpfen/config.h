dabd666a05e5814469f53e491c59f1e1e46c24c7
CharlesSchimmel <charlesschimmel@gmail.com>
2021-08-06 22:47:24 -0500

    update v60 build to use userspace stuff

--- keyboards/v60_type_r/keymaps/elpfen/config.h
+++ keyboards/v60_type_r/keymaps/elpfen/config.h
@@ -14,18 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
-
-#include "../../config.h"
+#pragma once
 
 #ifdef V60_POLESTAR
 #undef V60_POLESTAR
-#endif
-
-// For KC_GESC - ignore grave escape when those modifiers pressed.
-#define GRAVE_ESC_ALT_OVERRIDE
-#define GRAVE_ESC_CTRL_OVERRIDE
-#define GRAVE_ESC_GUI_OVERRIDE
-
 #endif
--- keyboards/v60_type_r/keymaps/elpfen/keymap.c
+++ keyboards/v60_type_r/keymaps/elpfen/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2017 REPLACE_WITH_YOUR_NAME
+/* Copyright 2017 elpfen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,42 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* This is the default ANSI layout provided by the KBP V60 Type R
-* as depicted in their manual and on the stock keycaps.
-*/
 #include QMK_KEYBOARD_H
 #include "elpfen.h"
-
-// LAYER DEFs
-#define _DEF 0
-#define _NAV 1
-#define _FNC 2
-#define _SYM 3
-
-// Mod Taps
-#define AL_3    ALT_T(KC_3)
-#define AL_8    RALT_T(KC_8)
-
-#define CT_2    CTL_T(KC_2)
-#define CT_9    RCTL_T(KC_9)
-
-// Layer Taps
-
-// Modded Keys
-#define SH_INS LSFT(KC_INS)
-
-// Quick Macros
-#define M_LOK LGUI(KC_L)
-#define M_CAD LCA(KC_DEL)
-#define M_CSE LCTL(LSFT(KC_ESC))
-
-// Tap Dance
-enum {
-  TD_ALTAB = 0
-};
-
-#define _____ KC_TRNS
-#define XXXXX KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap 0: Default Layer (Qwerty)
@@ -65,22 +31,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|MFnc|Gui |         SpSym           |RGui| Fn |Alt|Ctrl|
    * `-----------------------------------------------------------'
    */
-  [0] = LAYOUT_60_ansi(
-
-/*
-KC_GESC, KC_1,    CT_2,    AL_3,   KC_4,    KC_5,    KC_6,    KC_7, AL_8,    CT_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
-AL_TAB,  KC_QUOT, KC_COMM, KC_DOT, KC_P,    KC_Y,    KC_F,    KC_G, KC_C,    KC_R,   KC_L,    KC_LBRC, KC_RBRC, CT_BSLS, \
-CT_ESC,  KC_A,    KC_O,    KC_E,   KC_U,    KC_I,    KC_D,    KC_H, KC_T,    KC_N,   KC_S,    KC_MINS, AL_ENT,           \
-SH_TAB,  RS_SCLN,  KC_Q,    KC_J,   KC_K,    KC_X,    KC_B,    KC_M, KC_W,    KC_V,   RS_Z,   SH_BSP,                    \
-KC_LCTL, FN_MNU, KC_LGUI, LW_SPC, KC_RGUI, FN_MNU, KC_RALT, KC_RCTL
-*/
-
-KC_GESC, KC_1,    CT_2,    AL_3,   KC_4,    KC_5,    KC_6,    KC_7, AL_8,    CT_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
-AL_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, CT_BSLS, \
-CT_ESC,  KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, AL_ENT,           \
-SH_TAB,  RS_Z,   KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT, RS_SCLN,  SH_BSP,                    \
-KC_LCTL, FN_MNU,  KC_LGUI, LW_SPC, KC_RGUI, FN_MNU, KC_RALT, KC_RCTL
-           ),
+  [_BASE] = LAYOUT_60_ansi(
+    KC_GESC, KC_1,   KC_2,    KC_3,   KC_4,    KC_5,   KC_6,    KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+    AL_TAB,  KC_Q,   KC_W,    KC_E,   KC_R,    KC_T,   KC_Y,    KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, CT_BSLS,
+    CT_ESC,  KC_A,   KC_S,    KC_D,   KC_F,    KC_G,   KC_H,    KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, AL_ENT,
+    SH_TAB,  RS_Z,   KC_X,    KC_C,   KC_V,    KC_B,   KC_N,    KC_M, KC_COMM, KC_DOT, RS_SCLN, SH_BSP,
+    KC_LCTL, FN_MNU, KC_LGUI, LW_SPC, KC_RGUI, FN_MNU, KC_RALT, KC_RCTL
+  ),
 
   /* Keymap 1: Navigation, VIM
    * Parent: Default
@@ -98,21 +55,12 @@ KC_LCTL, FN_MNU,  KC_LGUI, LW_SPC, KC_RGUI, FN_MNU, KC_RALT, KC_RCTL
    * `-----------------------------------------------------------'
    */
   [_NAV] = LAYOUT_60_ansi(
-
-/*
-KC_GRV, _____,  _____,   _____,   KC_END,  _____, _____,   _____,   _____, _____,   KC_HOME, _____,   _____,   KC_DEL, \
-_____, MS_WUP, MS_BTN1, MS_UP,   MS_BTN2, _____, _____,   _____,   _____, _____,   KC_RGHT, KC_PGUP, KC_PGDN, KC_INS, \
-_____, MS_WDWN, MS_LEFT, MS_DOWN, MS_RGHT, _____, _____,   KC_LEFT, _____, _____,  _____,   _____,   _____,           \
-_____, RS_Z,  _____,   KC_DOWN, KC_UP,   _____, _____,   _____,   _____, _____,   RS_SCLN,  SH_DEL,                   \
-_____, _____,  _____,   _____,   _____,   _____, _____,   _____
-*/
-
-_____, _____,  _____,   _____,   KC_END,  _____, _____,   _____,   _____, _____,   KC_HOME, _____,   _____,   KC_DEL, \
-_____, MS_WUP, MS_BTN1, MS_UP,   MS_BTN2, _____, _____,   _____,   _____, _____,   M_PST,   KC_PGUP, KC_PGDN, KC_INS, \
-_____, MS_WDWN, MS_LEFT, MS_DOWN, MS_RGHT, _____, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _____,   _____,   _____,           \
-_____, RS_Z,  _____,   _____,   _____,   _____, _____,   _____,   _____, _____,   RS_SCLN,  SH_DEL,                   \
-_____, _____,  _____,   _____,   _____,   _____, _____,   _____
-           ),
+    _______, _______, _______, _______, KC_END,  _______, _______, _______, _______, _______, KC_HOME, _______, _______, KC_DEL,
+    _______, MS_WUP,  MS_BTN1, MS_UP,   MS_BTN2, _______, _______, _______, _______, _______, M_PST,   KC_PGUP, KC_PGDN, KC_INS,
+    _______, MS_WDWN, MS_LEFT, MS_DOWN, MS_RGHT, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
+    _______, RS_Z,    _______, _______, _______, _______, _______, _______, _______, _______, RS_SCLN, SH_DEL,
+    _______, _______, _______, _______, _______, _______, _______, _______
+  ),
 
   /* Keymap 2: Function, Media
    * Parent: Default
@@ -129,12 +77,12 @@ _____, _____,  _____,   _____,   _____,   _____, _____,   _____
    * `-----------------------------------------------------------'
    */
   [_FNC] = LAYOUT_60_ansi(
-KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
-_____,  M_LOK, M_CAD, M_CSE, _____, _____, _____, _____, _____,   _____,   KC_MPLY, KC_MPRV, KC_MNXT, _____,  \
-_____,  _____, _____, _____, _____, _____, _____, _____, _____,   _____,   _____,   _____,   _____,           \
-_____,  _____, _____, _____, _____, _____, _____, _____, KC_VOLD, KC_VOLU, KC_MUTE, SH_DEL,                   \
-_____,  _____, _____, _____, _____, _____, _____, _____
-           ),
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+    _______, M_LOK,   M_CAD,   M_CSE,   _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MPRV, KC_MNXT, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, SH_DEL,
+    _______, _______, _______, _______, _______, _______, _______, _______
+  ),
 
   /* Keymap 3: Symbols
    * Parent: Default
@@ -151,35 +99,18 @@ _____,  _____, _____, _____, _____, _____, _____, _____
    * `-----------------------------------------------------------'
    */
   [_SYM] = LAYOUT_60_ansi(
-KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
-_____,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,  _____,   KC_INS, \
-_____,   KC_LBRC, KC_LCBR, KC_LPRN, KC_MINS, KC_LABK, KC_RABK, KC_EQL,  KC_RPRN, KC_RCBR, KC_RBRC, _____,   _____,           \
-_____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,                    \
-_____,   _____,   _____,   XXXXX,   _____,   _____,   _____,   _____
-           )
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,  _______, KC_INS,
+    _______, KC_LBRC, KC_LCBR, KC_LPRN, KC_MINS, KC_LABK, KC_RABK, KC_EQL,  KC_RPRN, KC_RCBR, KC_RBRC, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, OOOOOOO, _______, _______, _______, _______
+  )
 
 };
 
 bool process_record_user(uint16_t current_keycode, keyrecord_t *record) {
-  return vim_windows_movement(current_keycode, record)
-      && dual_purpose_volume_keys(current_keycode, record)
+  return pseudo_twm(current_keycode, record)
+      && multi_purpose_volume_keys(current_keycode, record)
       && true;
 }
-
-void td_atab_fn(qk_tap_dance_state_t *state, void *user_data) {
-  if (state-> count == 1) {
-    SEND_STRING(SS_DOWN(X_LALT));
-    SEND_STRING(SS_TAP(X_TAB));
-  } else {
-    SEND_STRING(SS_TAP(X_TAB));
-  }
-}
-
-void td_atab_done_fn(qk_tap_dance_state_t *state, void *user_data) {
-  SEND_STRING(SS_UP(X_LALT));
-}
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_ALTAB] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(td_atab_fn, td_atab_done_fn, NULL, 300)
-};
 
