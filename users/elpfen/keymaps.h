802202b3f6c7bf0aa98cdaebf346edea4296c0b3
CharlesSchimmel <charlesschimmel@gmail.com>
2021-08-07 13:14:47 -0500

    clean up map defs a bit

--- users/elpfen/keymaps.h
+++ users/elpfen/keymaps.h
@@ -43,7 +43,7 @@
 // ~~~~~~~~~ Symbol Layer ~~~~~~~~~~
 #define _____________SYM_2U_Sides_12k______________ KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,  KC_F7,   KC_F8,   KC_F9, KC_F10, KC_F11
 
-#define _____________SYM_1U_Core_10k_______________ KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0
-#define ___1_through_10___
+#define ___1_through_10___ KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0
+#define _____________SYM_1U_Core_10k_______________ ___1_through_10___
 
 #define _____________SYM_Home_Core_10k_____________ KC_LBRC, KC_LCBR, KC_LPRN, KC_MINS, KC_LABK, KC_RABK, KC_EQL, KC_RPRN, KC_RCBR, KC_RBRC
