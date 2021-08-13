3fdf88ffb1e158366f9fe7c7d133f779eaf1afb9
CharlesSchimmel <charlesschimmel@gmail.com>
2021-08-12 22:11:23 -0500

    use userspace keymaps in v60

--- keyboards/v60_type_r/keymaps/elpfen/keymap.c
+++ keyboards/v60_type_r/keymaps/elpfen/keymap.c
@@ -17,75 +17,32 @@
 #include QMK_KEYBOARD_H
 #include "elpfen.h"
 
+#define LAYOUT_wrapper(...) LAYOUT_60_ansi(__VA_ARGS__)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap 0: Default Layer (Qwerty)
+  /* Dvorak
    * ,-----------------------------------------------------------.
-   * | ` | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = |   Bs  |
+   * | ` | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = |   \   |
    * |-----------------------------------------------------------|
-   * | ATb | Q | W | E | R | T | Y | U | I | O | P | [ | ] | Ct\ |
+   * |  `  | ' | , | . | P | y | f | g | c | r | l | / | = |  /  |
    * |-----------------------------------------------------------|
-   * | EsCtl | A | S | D | F | G | H | J | K | L | ; | ' | AlEnt |
+   * | EsCtl | a | o | e | u | i | d | h | t | n | s |-Ct| SyEnt |
    * |---------------------.-----------.-------------------------|
-   * | Shift  | Z | X | C | V | B | N | M | , | . | / |  SftBsp  |
+   * | ShTab  | ; | q | j | k | x | b | m | w | v | z |  SftBsp  |
    * |-----------------------------------------------------------|
    * |Ctrl|MFnc|Gui |         SpSym           |RGui| Fn |Alt|Ctrl|
    * `-----------------------------------------------------------'
    */
-  [_BASE] = LAYOUT_60_ansi(
-    KC_GESC, KC_1,   KC_2,    KC_3,   KC_4,    KC_5,   KC_6,    KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-    AL_TAB,  KC_Q,   KC_W,    KC_E,   KC_R,    KC_T,   KC_Y,    KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, CT_BSLS,
-    CT_ESC,  KC_A,   KC_S,    KC_D,   KC_F,    KC_G,   KC_H,    KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, AL_ENT,
-    SH_TAB,  RS_Z,   KC_X,    KC_C,   KC_V,    KC_B,   KC_N,    KC_M, KC_COMM, KC_DOT, RS_SCLN, SH_BSP,
-    KC_LCTL, FN_MNU, KC_LGUI, LW_SPC, KC_RGUI, FN_MNU, KC_RALT, KC_RCTL
+  [_BASE] = LAYOUT_wrapper(
+                      KC_GRV, _____________Dvorak_2Up_Core_10k___________, KC_LBRC, KC_RBRC, KC_BSLS,
+
+                      KC_GRV, _____________Dvorak_1Up_Core_10k___________, KC_SLSH, KC_EQL,  KC_BSLS,
+                              _____________Dvorak_Home_Sides_12k_________,                    LW_ENT,
+                              _____________Dvorak_1Dn_Sides_12k__________,
+    KC_LCTL, FN_MNU, KC_LGUI,                  LW_SPC,              KC_RGUI, FN_MNU, KC_RALT, KC_RCTL
   ),
 
-  /* Keymap 1: Navigation, VIM
-   * Parent: Default
-   *    Actvtr: Space
-   * ,-----------------------------------------------------------.
-   * | ` |   |   |   |End|   |   |   |   |   |Hom|   |   |  Del  |
-   * |-----------------------------------------------------------|
-   * |     |wu |lc | u | rc|   |   |   |   |   |SHn|PU|PgD| Ins  |
-   * |-----------------------------------------------------------|
-   * |      |wd | l | d | r |   | h | j | k | l |   |   |        |
-   * |--------------------.-----------.--------------------------|
-   * |        |xxx|   |   |   |    |    |   |   |   |xxx| SftDel |
-   * |-----------------------------------------------------------|
-   * |    |    |    |                        |    |    |    |    |
-   * `-----------------------------------------------------------'
-   */
-  [_NAV] = LAYOUT_60_ansi(
-    _______, _______, _______, _______, KC_END,  _______, _______, _______, _______, _______, KC_HOME, _______, _______, KC_DEL,
-    _______, MS_WUP,  MS_BTN1, MS_UP,   MS_BTN2, _______, _______, _______, _______, _______, M_PST,   KC_PGUP, KC_PGDN, KC_INS,
-    _______, MS_WDWN, MS_LEFT, MS_DOWN, MS_RGHT, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
-    _______, RS_Z,    _______, _______, _______, _______, _______, _______, _______, _______, RS_SCLN, SH_DEL,
-    _______, _______, _______, _______, _______, _______, _______, _______
-  ),
-
-  /* Keymap 2: Function, Media
-   * Parent: Default
-   * ,-----------------------------------------------------------.
-   * | ` | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  Del  |
-   * |-----------------------------------------------------------|
-   * |     |LOK|CAD|CSE|   |   |   |   |   |   |Ply|I<| >I|      |
-   * |-----------------------------------------------------------|
-   * |      |   |   |   |   |   |   |   |   |   |   |   |        |
-   * |--------------------.-----------.--------------------------|
-   * |        |   |   |   |   |    |    |   |VlD|VlU|Mut| SftDel |
-   * |-----------------------------------------------------------|
-   * |    |    |    |                        |XXXX|    |    |    |
-   * `-----------------------------------------------------------'
-   */
-  [_FNC] = LAYOUT_60_ansi(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-    _______, M_LOK,   M_CAD,   M_CSE,   _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MPRV, KC_MNXT, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, SH_DEL,
-    _______, _______, _______, _______, _______, _______, _______, _______
-  ),
-
-  /* Keymap 3: Symbols
-   * Parent: Default
+  /* Symbols
    * ,-----------------------------------------------------------.
    * | ` | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  Del  |
    * |-----------------------------------------------------------|
@@ -98,19 +55,84 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |          XXXX          |    |    |    |    |
    * `-----------------------------------------------------------'
    */
-  [_SYM] = LAYOUT_60_ansi(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-    _______,                         _____________SYM_1U_Core_10k_______________,                      KC_EQL,  _______, KC_INS,
+  [_SYM] = LAYOUT_wrapper(
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_DEL,
+    _______,                         _____________SYM_1Up_Core_10k______________,                      KC_EQL,  _______,  KC_INS,
     _______,                         _____________SYM_Home_Core_10k_____________,                               _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+    _______, _______, _______, _______, _______, _______,                            _____________SYM_1Dn_Sides_R_6k____________,
     _______, _______, _______,                        OOOOOOO,                                _______, _______, _______, _______
-  )
+  ),
+
+  /* Navigation
+   * ,-----------------------------------------------------------.
+   * | ` |   |   |   |End|   |   |   |   |   |Hom|   |   |  Del  |
+   * |-----------------------------------------------------------|
+   * |     |wu |lc | u | rc|   |   |   |   |   |SHn|PU|PgD| Ins  |
+   * |-----------------------------------------------------------|
+   * |      |wd | l | d | r |   | h | j | k | l |   |   |        |
+   * |--------------------.-----------.--------------------------|
+   * |        |xxx|   |   |   |    |    |   |   |   |xxx| SftDel |
+   * |-----------------------------------------------------------|
+   * |    |    |    |                        |    |    |    |    |
+   * `-----------------------------------------------------------'
+   */
+  [_NAV] = LAYOUT_wrapper(
+    _____________NAV_2Up_Sides_12k_____________, _______, KC_DEL,
+    _____________NAV_1Up_Sides_12k_____________, _______, KC_INS,
+    _____________NAV_Home_Sides_12k____________, _______,
+    _____________NAV_1Dn_Sides_12k_____________,
+    _______, _______, _______, _______, _______, _______, _______, _______
+  ),
+
+  /* Keymap 2: Function, Media
+   * Parent: Default
+   * ,-----------------------------------------------------------.
+   * | ` | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  Del  |
+   * |-----------------------------------------------------------|
+   * |     |VlD|VlU|Mut|   |   |   |PSc|   |   |Lok|  |   |      |
+   * |-----------------------------------------------------------|
+   * |      |   |   |   |Ulk|   |   |   |   |   |Slp| - |        |
+   * |--------------------.-----------.--------------------------|
+   * |        |   |   |   |   |    |    |   |   |Wak|   | SftDel |
+   * |-----------------------------------------------------------|
+   * |    |Rset|    |                        |    |oooo|    |    |
+   * `-----------------------------------------------------------'
+   */
+  [_FNC] = LAYOUT_wrapper(
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+    _____________FNC_1Up_Sides_12k_____________, _______, _______,
+    _____________FNC_Home_Sides_12k____________, _______,
+    _______, _______, _______, _______, _______, _______, _____________FNC_1Dn_Sides_R_6k____________,
+    _______, RESET, _______, _______, _______, OOOOOOO, _______, _______
+  ),
+
 
 };
 
-bool process_record_user(uint16_t current_keycode, keyrecord_t *record) {
-  return pseudo_twm(current_keycode, record)
-      && multi_purpose_volume_keys(current_keycode, record)
-      && true;
+layer_state_t layer_state_set_user(layer_state_t current_state) {
+    static layer_state_t previous_state;
+
+    current_state = lenient_update_tri_layer_state(current_state, _SYM, _NAV, _ADJ);
+
+    // Clear sticky mods: using multiple layers means that the mods will
+    // stay on so long as any of those layers is activated. May or may not
+    // be desired.
+    if (was_layer_turned_off(previous_state, current_state, _ADJ)) {
+        clear_mods();
+    }
+
+    current_state = switchboard_state(previous_state, current_state);
+    previous_state = current_state;
+
+    return current_state;
 }
 
+// ~~~~ Keypress Processing ~~~~~
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    return pseudo_twm(keycode, record)
+        && multi_purpose_volume_keys(keycode, record)
+        && layer_lock(keycode, record)
+        && vimmish_keys(keycode, record)
+        && layer_sticky_mods(keycode, record, _ADJ)
+        && true;
+}
