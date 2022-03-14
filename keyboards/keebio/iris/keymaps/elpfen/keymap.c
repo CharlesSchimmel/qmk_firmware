7b8fbecbc87de3412d3a6d03e2818de87d44b6ff
CharlesSchimmel <charlesschimmel@gmail.com>
2022-03-13 21:31:46 -0500

    more switchboard and sticky mod stuff

--- keyboards/keebio/iris/keymaps/elpfen/keymap.c
+++ keyboards/keebio/iris/keymaps/elpfen/keymap.c
@@ -157,11 +157,10 @@ layer_state_t layer_state_set_user(layer_state_t current_state) {
     return current_state;
 }
 
-bool clear_mods_after_adj(uint16_t keycode, keyrecord_t *record) {
+// must be called after clear_mods_after_adj
+bool switchboard_adj(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) return true;
-    if (keycode == AD_ENT || keycode == AD_SPC) {
-        clear_mods();
-    }
+    if (keycode == AD_ENT || keycode == AD_SPC) return false;
     return true;
 }
 
@@ -173,6 +172,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         && vimmish_keys(keycode, record)
         && layer_sticky_mods(keycode, record, _ADJ)
         && clear_mods_after_adj(keycode, record)
+        && switchboard_adj(keycode, record)
         ;
 }
 
--- users/elpfen/elpfen.h
+++ users/elpfen/elpfen.h
@@ -31,3 +31,7 @@ bool layer_sticky_mods(
         uint16_t current_keycode,
         keyrecord_t *record,
         layer_state_t layers);
+bool clear_mods_after_adj
+    ( uint16_t keycode
+    , keyrecord_t *record
+    );
