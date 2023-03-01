2cf0e8cb01f152e0b18bf31879aa9e3d33e58298
CharlesSchimmel <charlesschimmel@gmail.com>
2023-03-01 08:14:32 -0600

    update images

--- users/elpfen/elpfen.c
+++ users/elpfen/elpfen.c
@@ -223,3 +223,25 @@ __attribute__((weak)) layer_state_t layer_state_set_user(layer_state_t current_s
 __attribute__((weak)) bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return ELPFEN_DEFAULT_PROREC ;
 }
+
+#ifdef TAP_DANCE_ENABLE
+// Enums defined for all examples:
+enum {
+    TD_ESC_CAPS,
+    CT_EGG,
+    CT_FLSH,
+    CT_CLN,
+    X_CTL,
+};
+
+void dance_egg(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count >= 100) {
+        SEND_STRING("Safety dance!");
+        reset_tap_dance(state);
+    }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [CT_EGG] = ACTION_TAP_DANCE_FN(dance_egg),
+};
+#endif
